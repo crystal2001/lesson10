dce4475eea8de57ac6a102fb1532f77b620801c7
crystal2001 <43784510+crystal2001@users.noreply.github.com>
2018-12-18 10:09:23 -0500

    1

---
+++ problem1.py
@@ -0,0 +1,9 @@
+print('-' * 75)
+print('Life Decision Bot: ')
+life = input('Do you have any homework due today? ')
+if life == 'No':
+	print('Well you should probably waste some time by watching youtube for hours.')
+
+else: 
+	print('Well unless you want to fail I advise doing your work right now!')
+print('-' * 75)
---
+++ problem3.py
@@ -0,0 +1,10 @@
+print('-' * 75)
+print("Welcome to CLUB425, the most'lit' club in downtown ACTvF. Before you can enter, I need you to answer some questions... ")
+print()
+age = input('What is your age today? ')
+age = int(age)
+if age >= 21:
+	print('Wow, congrats on living long enough to come inside...')
+
+else:
+	print('Wow hold on youngling, you have to be 21 or older to enter.')
